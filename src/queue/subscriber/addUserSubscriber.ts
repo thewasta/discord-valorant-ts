e02abcc2833857db19a51e2b0fb76c2f680a3fbc
Schenier Lpez <schnierlopez@gmail.com>
2022-05-29 17:01:39 +0200

    Fix channel

--- src/queue/subscriber/addUserSubscriber.ts
+++ src/queue/subscriber/addUserSubscriber.ts
@@ -5,9 +5,9 @@ import utils from "../../util/utils";
 
 export default async function (job: Job, result: Error | any) {
     const data: iPayload = job.data;
+    const channel = await client.channels.fetch(data.channel as string);
 
     if (result instanceof Error) {
-        const channel = await client.channels.fetch(data.channel as string);
         const transMessage = utils.translate("messages.game_tag_not_found", data.guild);
         const message = transMessage.replace("_REPLACE_", data.payload.gameTag);
         //@ts-ignore
